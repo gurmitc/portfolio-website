d4ebb944eb6063a46d744c743206c126f1033d40
Gurmit Chauhan <gurmit.chauhan@leaplegalsoftware.ca>
2025-02-26 16:57:00 -0500

    removed unused vars

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import Intro from "@/components/intro";
-import { MDXRemote } from "next-mdx-remote/rsc";
 import RecentProjects from '@/components/recent-projects';
 
 export default function Home() {
-  const content = `
-        # this is markdown heading
-        `
   
   return (
     <section className='pb-24 pt-40'>
