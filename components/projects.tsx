a8e290a92a933f32298d51ea33867f06d44a2060
Gurmit Chauhan <gurmit.chauhan@leaplegalsoftware.ca>
2025-03-14 14:23:28 -0400

    added different link for journal project

--- components/projects.tsx
+++ components/projects.tsx
@@ -13,7 +13,7 @@ export default function Projects({
     <ul className='grid grid-cols-1 gap-8 sm:grid-cols-2'>
       {projects.map(project => (
         <li key={project.slug} className='group relative'>
-          <Link href={`/projects/${project.slug}`}>
+          <Link href={`/projects/${project.slug}` == "/projects/journal-entry-tracker" ? "https://gurmit-journal-entry.vercel.app/" : `/projects/${project.slug}`}>
             {project.image && (
               <div className='h-72 w-full overflow-hidden bg-muted sm:h-60'>
                 <Image
@@ -29,7 +29,7 @@ export default function Projects({
 
             <div className='absolute inset-x-0 bottom-0 translate-y-2 px-6 py-5 opacity-0 transition-all duration-500 group-hover:translate-y-0 group-hover:opacity-100'>
               <h2 className='title line-clamp-1 text-xl no-underline'>
-                {project.title}
+              {project.title}
               </h2>
               <p className='line-clamp-1 text-sm text-muted-foreground'>
                 {project.summary}
